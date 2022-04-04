bae6f06121f94b98aae5cd35524bee28199ec9ab
Julian Lafratta <julian.lafratta@gmail.com>
2022-04-04 09:10:27 -0300

    testFindByIdOk

--- product-service/src/test/java/edu/store/product/service/ProductServiceTest.java
+++ product-service/src/test/java/edu/store/product/service/ProductServiceTest.java
@@ -12,6 +12,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -34,11 +35,15 @@ class ProductServiceTest {
         categoryMock = new CategoryMock();
         productMock = new ProductMock(categoryMock);
 
+        Product defaultProduct = productMock.getOneComplete();
         Category defaultCategory = categoryMock.getDefaultComplete();
         List<Product> defaultListByCategory = productMock.getCompleteListDefaultCategory();
 
         Mockito.when(productRepository.findByCategory(defaultCategory))
                 .thenReturn(defaultListByCategory);
+
+        Mockito.when(productRepository.findById(1L))
+                .thenReturn(Optional.of(defaultProduct));
     }
 
     @Test
@@ -48,5 +53,12 @@ class ProductServiceTest {
 
         assertTrue(products.stream().allMatch(product -> product.getCategory().equals(defaultCategory)));
         verify(productRepository, times(1)).findByCategory(defaultCategory);
+    }
+
+    @Test
+    void testFindByIdOk() {
+        Product found = productService.findById(1L);
+        assertEquals(found.getId(), 1L);
+        assertEquals(found.getName(), "Impresora 3D Artillery Genius");
     }
 }
